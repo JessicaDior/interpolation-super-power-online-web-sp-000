978aa4ecb1411c50bc3480ad2ccc97648047ff10
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-04 02:16:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-def display_rainbow
-  puts "#{colors}"
+def display_rainbow(display_rainbow = "colors")
+  puts "#{display_rainbow}"
 end
